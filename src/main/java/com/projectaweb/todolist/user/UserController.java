cc90fe688b4e39f989044a5d52d49d0a88afc3ee
Ramon Ryon <ramonryon@gmail.com>
2023-10-16 16:25:19 -0300

    Creating ResponseEntity

--- src/main/java/com/projectaweb/todolist/user/UserController.java
+++ src/main/java/com/projectaweb/todolist/user/UserController.java
@@ -1,6 +1,8 @@
 package com.projectaweb.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,9 +16,14 @@ public class UserController {
     private IUserRepository userRepository;
 
     @PostMapping("/")
-    public UserModel create(@RequestBody UserModel userModel) {
-        var userCreated = this.userRepository.save(userModel);
-        return userCreated;
-    }
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
 
+        if (user != null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+        }
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+    }
 }
